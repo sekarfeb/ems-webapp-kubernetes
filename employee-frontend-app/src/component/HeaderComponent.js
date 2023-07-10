4270100c12480c8b2209828e973842dc1f03b21f
sekarfeb <sekarfeb@gmail.com>
2023-07-10 18:23:01 +0530

    changes

--- employee-frontend-app/src/component/HeaderComponent.js
+++ employee-frontend-app/src/component/HeaderComponent.js
@@ -6,7 +6,7 @@ const HeaderComponent = () => {
             <header>
                 <nav className='navbar navbar-expand-md navbar-dark bg-dark p-3'>
                     <div>
-                        <a className='navbar-brand m-5' href="">Employee Management System</a>
+                        <a className='navbar-brand m-5' href="">Employee Management System - V2</a>
                     </div>
                 </nav>
             </header>
